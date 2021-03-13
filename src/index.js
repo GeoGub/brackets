5ee4015ae73671f030c33c8c99f046d406dd9de4
GeorgiyGubarkov <g.v.gubarkov@mpt.ru>
2021-03-13 14:41:22 +0300

    feat: add | brackets

--- src/index.js
+++ src/index.js
@@ -13,15 +13,15 @@ module.exports = function check(str, bracketsConfig) {
       '|': '|',
     }
   }
-
+  console.log(brackets.data)
   brackets.data.forEach(thisChar => {
-    if ((thisChar === '(') || (thisChar === '[') || (thisChar === '{')) {
+    if (brackets.bracket[thisChar] === brackets.lastChar) {
+      brackets.lastChar = brackets.prviusChars.shift()
+    } else if ((thisChar === '(') || (thisChar === '[') || (thisChar === '{' || thisChar === '|')) {
       brackets.prviusChars.unshift(brackets.lastChar)
       brackets.lastChar = thisChar
     } else if (brackets.bracket[thisChar] != brackets.lastChar) {
       brackets.status = false
-    } else if (brackets.bracket[thisChar] === brackets.lastChar) {
-      brackets.lastChar = brackets.prviusChars.shift()
     }
   });
   if ((brackets.status === false) || (brackets.lastChar != null)) {
