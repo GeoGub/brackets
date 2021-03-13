bfc8d707761664d235d0f7440de5cb1cca8bbd9b
GeorgiyGubarkov <g.v.gubarkov@mpt.ru>
2021-03-13 14:28:55 +0300

    feat: add all brackets without |

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  let brackets = {
+    status: true,
+    lastChar: null,
+    data: str.split(''),
+    prviusChars: [], 
+    bracket: {
+      ')': '(',
+      ']': '[',
+      '}': '{',
+      '|': '|',
+    }
+  }
+
+  brackets.data.forEach(thisChar => {
+    if ((thisChar === '(') || (thisChar === '[') || (thisChar === '{')) {
+      brackets.prviusChars.unshift(brackets.lastChar)
+      brackets.lastChar = thisChar
+    } else if (brackets.bracket[thisChar] != brackets.lastChar) {
+      brackets.status = false
+    } else if (brackets.bracket[thisChar] === brackets.lastChar) {
+      brackets.lastChar = brackets.prviusChars.shift()
+    }
+  });
+  if ((brackets.status === false) || (brackets.lastChar != null)) {
+    return false
+  }
+  return true
 }
